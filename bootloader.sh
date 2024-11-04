9bb783afdb1306c946aa9f5af6e737ebda33368a
gmicroul <121873438+gmicroul@users.noreply.github.com>
2024-11-04 16:55:30 +0800

    Update bootloader.sh

--- bootloader.sh
+++ bootloader.sh
@@ -38,7 +38,7 @@ for variant in ${DTB_VARIANTS}; do
     abootimg --create ./openSUSE-Tumbleweed-ARM-PHOSH-${DEVICE}${variant}.aarch64.boot.img -c kerneladdr=0x8000 \
         -c ramdiskaddr=0x1000000 -c secondaddr=0x0 -c tagsaddr=0x100 -c pagesize=4096 \
         -c cmdline="BOOT_IMAGE=/boot/Image root=${ROOTPART} quiet splash" \
-        -k /tmp/kernel-dtb -r ${MOUNTED_IMAGE_DIR}/boot/initrd
+        -k /tmp/kernel-dtb -r ${MOUNTED_IMAGE_DIR}/boot/initrd-6.11.0-32.gb87e886-sdm845
 
     #mkbootimg --kernel ${MOUNTED_IMAGE_DIR}/boot/Image --dtb ${MOUNTED_IMAGE_DIR}/boot/dtb/qcom/sdm845-${DTB_VENDOR}-${variant}.dtb --pagesize 4096 \
     #    --base 0x00000000 --kernel_offset 0x00008000 --second_offset 0x00f00000 --tags_offset 0x00000100 \
